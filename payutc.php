13629cdd8ebce0dbbc72dc60dbbcb346cb0043e8
Matthieu Guffroy <mattgu74@gmail.com>
2014-05-07 16:10:01 +0200

    wip

--- payutc.php
+++ payutc.php
@@ -7,6 +7,8 @@ include_once 'config.inc.php';
 
 session_start();
 
+$debut = "2014-05-06 18:30";
+
 if(!isset($_SESSION['payutc_cookie'])) {
 	// Cookie pas défini => login app
 	$payutcClient = new AutoJsonClient($_CONFIG['payutc_server'], "STATS");
@@ -18,10 +20,20 @@ if(!isset($_SESSION['payutc_cookie'])) {
 }
 
 try {
-	echo json_encode($payutcClient->getRevenue(array("fun_id" => 2, "start"=> "2014-05-06 18:30")));
+	$total = $payutcClient->getRevenue(array("fun_id" => 2, "start"=> $debut));
 } catch(Exception $e) {
 	$payutcClient->loginApp(array("key" => $_CONFIG['payutc_key']));
 	$_SESSION['payutc_cookie'] = $payutcClient->cookie;
-	echo json_encode($payutcClient->getRevenue(array("fun_id" => 2, "start"=> "2014-05-06 18:30")));
+	$total = $payutcClient->getRevenue(array("fun_id" => 2, "start"=> $debut));
 }
 
+return json_encode(array(
+    "total" => $total,
+    "GI" => $payutcClient->getNbSell(array("fun_id" => 2, "start"=> $debut, "obj_id" => "1702")),
+    "TC" => $payutcClient->getNbSell(array("fun_id" => 2, "start"=> $debut, "obj_id" => "1703")),
+    "GSU" => $payutcClient->getNbSell(array("fun_id" => 2, "start"=> $debut, "obj_id" => "1704")),
+    "GM" => $payutcClient->getNbSell(array("fun_id" => 2, "start"=> $debut, "obj_id" => "1705")),
+    "GSM" => $payutcClient->getNbSell(array("fun_id" => 2, "start"=> $debut, "obj_id" => "1706")),
+    "GB" => $payutcClient->getNbSell(array("fun_id" => 2, "start"=> $debut, "obj_id" => "1707")),
+    "GP" => $payutcClient->getNbSell(array("fun_id" => 2, "start"=> $debut, "obj_id" => "1708"))
+   ));
