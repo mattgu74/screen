49d097be3f7c349257212b43608e57fe0b904f73
Matthieu Guffroy <mattgu74@gmail.com>
2014-05-07 02:19:21 +0200

    payutc OK

---
+++ config.inc.dist.php
@@ -0,0 +1,6 @@
+<?php
+
+$_CONFIG = array();
+
+$_CONFIG['payutc_server'] = "https://assos.utc.fr/payutc_dev/server/";
+$_CONFIG['payutc_key'] = "";
---
+++ payutc.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+use \Payutc\Client\AutoJsonClient;
+
+include_once 'config.inc.php';
+
+session_start();
+
+if(!isset($_SESSION['payutc_cookie'])) {
+	// Cookie pas défini => login app
+	$payutcClient = new AutoJsonClient($_CONFIG['payutc_server'], "STATS");
+	// Login app
+	$payutcClient->loginApp(array("key" => $_CONFIG['payutc_key']));
+	$_SESSION['payutc_cookie'] = $payutcClient->cookie;
+} else {
+	$payutcClient = new AutoJsonClient($_CONFIG['payutc_server'], "STATS", array(), "Payutc Json PHP Client", $_SESSION['payutc_cookie']);
+}
+
+echo json_encode($payutcClient->getRevenue(array("fun_id" => 2, "start"=> "2014-05-06 18:30")));
+
