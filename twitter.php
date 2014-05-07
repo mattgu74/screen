47fe36466489d408836f16a5871f29d5f108b889
Matthieu Guffroy <mattgu74@gmail.com>
2014-05-07 15:16:11 +0200

    wip

--- twitter.php
+++ twitter.php
@@ -10,7 +10,7 @@ $settings = array(
 );
 
 $url = 'https://api.twitter.com/1.1/search/tweets.json';
-$getfield = '?q=%23PicassoUTC&result_type=recent&count=10';
+$getfield = '?q=%23PicassoUTC,%40picasso_utc&result_type=recent&count=10';
 $requestMethod = 'GET';
 
 $twitter = new TwitterAPIExchange($settings);
